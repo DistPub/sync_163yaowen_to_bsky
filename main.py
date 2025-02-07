d01e40beabf7713b99af12052945cf9cbae49e4d
robot auto <xiaopengyou@live.com>
2025-02-07 15:02:55 +0000

    try fix

--- main.py
+++ main.py
@@ -36,6 +36,8 @@ def is_later_news(news_time, pre_news_time):
 
 
 def fetch_img(url):
+    import os
+    os.system(f'curl -v {url}')
     response = requests.get(url, headers={
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36"
     })
