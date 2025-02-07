a147e3dba82816a8ed685f478175c05cfb03ba4a
robot auto <xiaopengyou@live.com>
2025-02-07 10:21:09 +0000

    update pre news time

--- main.py
+++ main.py
@@ -36,7 +36,9 @@ def is_later_news(news_time, pre_news_time):
 
 
 def fetch_img(url):
-    response = requests.get(url)
+    response = requests.get(url, headers={
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36"
+    })
     response.raise_for_status()
     return response.content
 
